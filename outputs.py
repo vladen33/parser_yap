a70156b491a45a630c6f6662af60e1add08929fa
Vladislav Vasilev <vladen21@yandex.ru>
2025-02-02 19:13:51 +0300

    Commit 04

--- outputs.py
+++ outputs.py
@@ -1,8 +1,14 @@
+import datetime as dt
 from prettytable import PrettyTable
 
+from constants import BASE_DIR, DATETIME_FORMAT
+
 def control_output(results, cli_args):
-    if cli_args.pretty:
+    output = cli_args.output
+    if output == 'pretty':
         pretty_output(results)
+    elif output == 'file':
+        file_output(results, cli_args)
     else:
         default_output(results)
 
@@ -18,3 +24,14 @@ def pretty_output(results):
     # Добавляем все строки, начиная со второй (с индексом 1).
     table.add_rows(results[1:])
     print(table)
+
+def file_output(results, cli_args):
+    results_dir = BASE_DIR / 'results'
+    results_dir.mkdir(exist_ok=True)
+    parser_mode = cli_args.mode
+    now = dt.datetime.now()
+    now_formatted = now.strftime(DATETIME_FORMAT)
+    file_name = f'{parser_mode}_{now_formatted}.csv'
+    file_path = results_dir / file_name
+
+    pass
